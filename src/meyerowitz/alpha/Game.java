0adc06749f03e9e05e3d2e787cbd3ff194919693
Alex Meyerowitz <alexandermeyerowitz@yahoo.com>
2016-07-10 03:59:58 -0400

    Allowed placement of shapes
    Gave hitboxes to every tile on the board

--- src/meyerowitz/alpha/Game.java
+++ src/meyerowitz/alpha/Game.java
@@ -3,7 +3,6 @@ package meyerowitz.alpha;
 import java.awt.*;
 import java.awt.event.*;
 import java.util.concurrent.*;
-
 import javax.swing.*;
 
 public class Game extends JPanel implements MouseListener
@@ -12,6 +11,7 @@ public class Game extends JPanel implements MouseListener
 	
 	private Tile[][] board;
 	private Shape[] shapes;
+	private Rectangle[][] hitbox;
 	
 	public Game()
 	{
@@ -21,6 +21,16 @@ public class Game extends JPanel implements MouseListener
 		for(int i = 0; i < 10; i++)
 			for(int j = 0; j < 10; j ++)
 				board[i][j] = new Tile();
+		
+		// create hitbox for the board (one for every tile)
+		hitbox = new Rectangle[10][10];
+		for(int i = 0; i < 10; i++)
+			for(int j = 0; j < 10; j++)
+			{
+				int xOffset = offsetCoords(i, board[i][j]);
+				int yOffset = offsetCoords(j, board[i][j]) + 80;
+				hitbox[i][j] = new Rectangle(xOffset, yOffset, board[i][j].getSize(), board[i][j].getSize());
+			}
 		
 		shapes = new Shape[3];
 		
@@ -99,9 +109,9 @@ public class Game extends JPanel implements MouseListener
 				if(tiles[i][j] != null)
 				{
 					int xOffset = (int) (offsetCoords(i, tiles[i][j]) + 
-							(MouseInfo.getPointerInfo().getLocation().getX()) - this.getLocationOnScreen().getX()) - 5;
+							(MouseInfo.getPointerInfo().getLocation().getX()) - this.getLocationOnScreen().getX()) - 10;
 					int yOffset = (int) (offsetCoords(j, tiles[i][j]) + 
-							(MouseInfo.getPointerInfo().getLocation().getY()) - this.getLocationOnScreen().getY()) - 5;
+							(MouseInfo.getPointerInfo().getLocation().getY()) - this.getLocationOnScreen().getY()) - 10;
 					g2D.setColor(tiles[i][j].getColor());
 					g2D.fillRoundRect(xOffset, yOffset, tiles[i][j].getSize(), tiles[i][j].getSize(), 10, 10);
 				}			
@@ -131,21 +141,56 @@ public class Game extends JPanel implements MouseListener
 	
 	public void mousePressed(MouseEvent e) 
 	{
-		System.out.println("mouse pressed");
 		Point point = new Point(e.getX(), e.getY());
 		
 		for(Shape shape: shapes)
-			if(shape.contains(point))
+			if(shape != null && shape.contains(point))
 			{
 				shape.setLifted(true);	
-				System.out.println("lifted shape");
 			}
 	}
 
 	public void mouseReleased(MouseEvent e) 
 	{
+		Point point = new Point(e.getX(), e.getY());
+		for(int i = 0; i < 10; i++)
+			for(int j = 0; j < 10; j++)
+				if(hitbox[i][j].contains(point))
+				{
+					int index = 0;
+					for(int a = 0; a < 3; a++)
+						if(shapes[a] != null && shapes[a].getLifted())
+							index = a;
+					
+					boolean placeable = false;
+					for(int a = 0; a < shapes[index].getTiles().length; a++)
+						for(int b = 0; b < shapes[index].getTiles().length; b++)
+							if(shapes[index].getTiles()[a][b] != null & ((i + a) < 10 & (j + b) < 10))
+								placeable = board[i + a][j + b].getFilled() ? false : true;
+					
+					if(placeable)
+					{
+						for(int a = 0; a < shapes[index].getTiles().length; a++)
+							for(int b = 0; b < shapes[index].getTiles().length; b++)
+								if(shapes[index].getTiles()[a][b] != null)
+								{
+									board[i + a][j + b].setColor(shapes[index].getTiles()[a][b].getColor());
+									board[i + a][j + b].setFilled(true);
+								}
+						shapes[index] = null;
+					}
+				}
+		
 		for(Shape shape: shapes)
-			if(shape.getLifted())
+			if(shape != null && shape.getLifted())
 				shape.setLifted(false);
-	}
+		
+		boolean noShapes = true;
+		for(Shape shape: shapes)
+			if(shape != null)
+				noShapes = false;
+		if(noShapes)
+			for(int i = 0; i < 3; i++)
+				shapes[i] = new Shape(i);
+		}
 }
